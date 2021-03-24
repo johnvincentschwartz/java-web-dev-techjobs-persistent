cee6eb72e341276f4242283c0991053292433c08
John Schwartz <johnvincentschwartz@gmail.com>
2021-03-24 09:25:24 -0500

    Fixed jobs on homepage

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 ## Part 1: Test it with SQL
-SELECT * FROM techjobs.job;
+SELECT id, name, employer_id FROM techjobs.job;
 
 ## Part 2: Test it with SQL
 SELECT * FROM techjobs.employer
@@ -9,3 +9,7 @@ WHERE location = "St. Louis";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY skill.name
